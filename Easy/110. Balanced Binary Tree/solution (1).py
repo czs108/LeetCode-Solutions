a226e8564884569b7b3a5e6f1eca1db757d8a0cc
Chenzs108 <chenzs108@outlook.com>
2020-06-01 09:27:17 +0800

    Feat: Add #110, #111

---
+++ Easy/110. Balanced Binary Tree/solution (1).py
@@ -0,0 +1,29 @@
+# 110. Balanced Binary Tree
+
+# Runtime: 3232 ms, faster than 5.10% of Python3 online submissions for Balanced Binary Tree.
+
+# Memory Usage: 17.6 MB, less than 100.00% of Python3 online submissions for Balanced Binary Tree.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    # Recursive
+    def isBalanced(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        else:
+            deep_differ = abs(Solution._getDeep(root.left) - Solution._getDeep(root.right)) <= 1
+            balance_child = self.isBalanced(root.left) and self.isBalanced(root.right)
+            return deep_differ and balance_child
+
+    @staticmethod
+    def _getDeep(node: TreeNode) -> int:
+        if not node:
+            return 0
+        else:
+            return max(Solution._getDeep(node.left), Solution._getDeep(node.right)) + 1
---
+++ Easy/111. Minimum Depth of Binary Tree/solution (1).py
@@ -0,0 +1,23 @@
+# 111. Minimum Depth of Binary Tree
+
+# Runtime: 48 ms, faster than 42.05% of Python3 online submissions for Minimum Depth of Binary Tree.
+
+# Memory Usage: 15.5 MB, less than 51.35% of Python3 online submissions for Minimum Depth of Binary Tree.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        elif not root.left:
+            return self.minDepth(root.right) + 1
+        elif not root.right:
+            return self.minDepth(root.left) + 1
+        else:
+            return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
